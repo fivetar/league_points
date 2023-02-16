42db9d25f8b8c110eb29d644e42bddf0928c1aea
fivetar <122467816+fivetar@users.noreply.github.com>
2023-02-16 15:52:21 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,16 @@
 def league_points(wins, draws, losses):
-    ...
 
+ wins = 5 * wins
+ draws = 0 * draws
+ losses = 2 * losses
+ 
+ total = wins + draws + losses
+ return total
+ 
+ 
+ 
+print(league_points(3, 4, 2))
+ 
 
 
 
